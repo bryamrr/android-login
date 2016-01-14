b53bd5039b09629d75b6c46f421d8bf17b4dcce6
brodriguezaybar <brodriguezaybar@gmail.com>
2016-01-14 10:45:00 -0500

    Add login with parse

--- DemoLogin/app/src/main/java/la/devacademy/demologin/MainActivity.java
+++ DemoLogin/app/src/main/java/la/devacademy/demologin/MainActivity.java
@@ -1,12 +1,24 @@
 package la.devacademy.demologin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mSignInButton;
+    private EditText mUsername;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +29,34 @@ public class MainActivity extends AppCompatActivity {
 
         Parse.initialize(this);
 
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        mSignInButton = (Button) findViewById(R.id.signInButton);
+        mUsername = (EditText) findViewById(R.id.usernameEditText);
+        mPassword = (EditText) findViewById(R.id.passwordEditText);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+
+        String username = mUsername.getText().toString();
+        String password = mPassword.getText().toString();
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                    Log.d("MainActivity", "Login failed!");
+                }
+            }
+        });
 
     }
 }
